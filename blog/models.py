eeefa92c75d0f0416f886299c4b8e8c4a8828e62
jaiv <jaiv.maurya@gmail.com>
2020-08-13 18:28:48 -0400

    Add a comment on a  blog post

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
 
 class Post(models.Model):
@@ -14,6 +15,18 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
-#    Create a string representation
+    #    Create a string representation
     def __str__(self):
         return str(self.title) + ' (' + str(self.author) + ')'
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    # Post.objects.get(pk =2).comments.all() will give all the comments of that perticular post comments.all() thanks to
+    # that related name attribute
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return self.text
