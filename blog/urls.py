f091d7ca183082034bb7880016ecad19bad7dc5e
jaiv <jaiv.maurya@gmail.com>
2020-08-17 18:26:01 -0400

    Added feature to approve comments

--- blog/urls.py
+++ blog/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
     path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
     path('post/<int:pk>', views.post_detail, name='post_detail'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
 ]
